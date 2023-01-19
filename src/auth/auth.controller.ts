a84e57c5c9c2c3f7b4a90018eb3bfc8ea1046015
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2023-01-19 09:55:10 +0100

    Basic controller returning text

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller({})
+@Controller('auth')
 export class AuthController {
     // Dependency injection
     constructor(private authService: AuthService) {
+    }
+
+    // POST auth/signup
+    @Post('signup')
+    signup() {
+        // Returned datatype is auto converted
+        return 'I am signed up'
+    }
+
+    // POST auth/signin
+    @Post('signin')
+    signin() {
+        return 'I am signed in'
     }
 }
