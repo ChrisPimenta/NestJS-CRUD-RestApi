bf419a0cdacd78ba005d2c3df8d1131ee861d462
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2023-01-29 15:40:01 +0100

    Set up e2e testing structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,11 +21,18 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/quotes": [
+      "error",
+      "single",
+      {
+        "allowTemplateLiterals": true
+      }
+    ],
   },
   'prettier/prettier': [
     'error',
     {
       'endOfLine': 'auto',
-    }
+    },
   ]
 };
