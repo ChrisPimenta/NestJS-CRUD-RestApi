09904d2fac9a6d66d005967eae6ac7bde081df09
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2023-01-29 15:58:15 +0100

    Test env setup

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,29 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 
+
 describe('App e2e', () => {
+    let app: INestApplication;
+
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
+        app = moduleRef.createNestApplication();
+        app.useGlobalPipes(
+            new ValidationPipe({
+                // Only use elements which are specified. No extra variables.
+                whitelist: true
+            })
+        );
+
+        await app.init();
     });
+
+    afterAll(() => {
+        app.close();
+    })
 
     it.todo('should pass');
 })
